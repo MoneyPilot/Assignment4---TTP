a33d5415e7b0f5772477ea8c49606643664be755
Danil <94500841+MoneyPilot@users.noreply.github.com>
2022-01-10 09:49:34 -0500

    Update PasswordCheck.js

--- PasswordCheck.js
+++ PasswordCheck.js
@@ -1,5 +1,11 @@
 //question #4
 
+let email = document.getElementById("exampleInputEmail1")
+let b = document.querySelector("button")
+let elem_h1 = document.createElement("h1")
+
+let check = document.forms[exampleInputEmail1].value;
+
 if (pass != 12345678){
     alert("Wrong password.Please try again!");
-}a
+}
